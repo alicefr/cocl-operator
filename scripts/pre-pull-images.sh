e71f4bcbab5fbd9a918d345bb551191ffd6b8de7
Alice Frosi <afrosi@redhat.com>
2026-01-21 14:20:59 +0100

    Avoid pre pull of coreos image
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- scripts/pre-pull-images.sh
+++ scripts/pre-pull-images.sh
@@ -12,7 +12,7 @@ IMAGES=(
 	"quay.io/kubevirt/virt-controller:${KV_VERSION}"
 	"quay.io/kubevirt/virt-operator:${KV_VERSION}"
 	"$TRUSTEE_IMAGE"
-	"$APPROVED_IMAGE"
+#	"$APPROVED_IMAGE"
 	"quay.io/trusted-execution-clusters/fedora-coreos-kubevirt:20260106"
 )
 
